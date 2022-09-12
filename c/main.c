b40fd27ffb409462ab1908df107b6e6d95243b93
kaikySantos <info.kaiky@gmail.com>
2022-09-12 14:08:13 -0300

    feat: stack push function

--- c/main.c
+++ c/main.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXTAM 100
+#define MAXTAM 200
 
 typedef struct {
   int top;
   float data[MAXTAM];
 } Stack;
 
+int stackIsFull(Stack *stack) {
+  if (stack->top == MAXTAM - 1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void push(Stack *stack, int newValue) {
+  if (stackIsFull(stack) == 1) {
+    printf("ERROR: Stack is full!");
+  } else {
+    stack->top++;
+    stack->data[stack->top] = newValue;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   char expression[] = {"5 3 2 + * 4 / 6 -"};
+
+  Stack stack;
+  stack.top = -1;
 
   return 0;
 }
