a5dac94f89c978cc0cd55a5b6d6db83d9151fab3
kaikySantos <info.kaiky@gmail.com>
2022-09-12 13:58:51 -0300

    feat: stack struct

--- c/main.c
+++ c/main.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXTAM 100
+
+typedef struct {
+  int top;
+  float data[MAXTAM];
+} Stack;
+
 int main(int argc, char const *argv[]) {
-  printf("Hello!");
+  char expression[] = {"5 3 2 + * 4 / 6 -"};
+
   return 0;
 }
